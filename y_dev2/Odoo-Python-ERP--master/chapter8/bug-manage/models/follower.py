93ba62e04645039f9415e6b77839df95ecb4aa72
dwc5lee <hnbbb@foxmail.com>
2024-10-05 21:51:37 +0800

    20241005-更新

--- y_dev2/Odoo-Python-ERP--master/chapter8/bug-manage/models/follower.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class follower(models.Model):
-    _inherit='res.partner'
-    bug_ids=fields.Many2many('bm.bug',string='bug')
--- y_dev2/Odoo-Python-ERP--master/chapter9/bm-advanced/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import bug_adv
-from . import bug_stage
-from . import bug_tag
